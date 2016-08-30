a4af8662e1848b27550035b22eab441b171a4334
大师兄 <dsx@itcast.cn>
2016-08-30 17:12:00 +0800

    🐂X

--- WeChat/WeChat/ViewController.m
+++ WeChat/WeChat/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"又一个震惊世界App要诞生了");
+    
 }
 
 - (void)didReceiveMemoryWarning {
