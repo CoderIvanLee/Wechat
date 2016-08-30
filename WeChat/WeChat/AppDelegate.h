8a327e1100f9c49ba443bdd5fd870f550caf494b
小师妹 <xsm@itcast.cn>
2016-08-30 17:24:20 +0800

    提交代码

--- WeChat/WeChat/AppDelegate.h
+++ WeChat/WeChat/AppDelegate.h
@@ -7,11 +7,11 @@
 //
 /**
  *  如果你新建文件夹,但是文件夹下没有文件,Git是不会提交的这个真是文件夹.但是项目的目录上有个文件.
- *
- *  @param strong    <#strong description#>
- *  @param nonatomic <#nonatomic description#>
- *
- *  @return <#return value description#>
+     
+    Xcode解决项目文件冲突
+    在冲突的地方,进行合并: 
+    我们很难看懂项目文件里面的配置是做什么的.
+ 
  */
 #import <UIKit/UIKit.h>
 
