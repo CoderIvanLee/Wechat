90d7e5bd968bbcdb1e8d427db8f487ad5dc17c5d
小师妹 <xsm@itcast.cn>
2016-08-30 17:19:51 +0800

    修改项目的目录

--- WeChat/WeChat/AppDelegate.h
+++ WeChat/WeChat/AppDelegate.h
@@ -5,7 +5,14 @@
 //  Created by iOS on 16/8/30.
 //  Copyright © 2016年 iOS. All rights reserved.
 //
-
+/**
+ *  如果你新建文件夹,但是文件夹下没有文件,Git是不会提交的这个真是文件夹.但是项目的目录上有个文件.
+ *
+ *  @param strong    <#strong description#>
+ *  @param nonatomic <#nonatomic description#>
+ *
+ *  @return <#return value description#>
+ */
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
